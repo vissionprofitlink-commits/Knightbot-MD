62f09eae26adcb46befd7e38dc995d8348a561b2
vissionprofitlink-commits <vissionprofitlink@gmail.com>
2025-12-30 17:37:25 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'lulu', // Your name
-  ownerNumber: '27608185720', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '27825274537', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
